eaf55870f3427275980fcd3a67aecf673dab1031
LianSheng197 <ci.i.cxcvii@gmail.com>
2021-01-22 09:58:29 +0800

    trigger webhook

---
+++ index.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         Simple Copymanga Downloader
+// @namespace    -
+// @version      0.0.2-init
+// @description  沒什麼技術成分，非常暴力的下載器
+// @author       LianSheng
+// @match        https://www.copymanga.com/*
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.5.0/jszip.min.js
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
