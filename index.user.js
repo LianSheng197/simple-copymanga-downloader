806324d4ad1471ade38be3fe6aa8f337a6c23c46
LianSheng197 <ci.i.cxcvii@gmail.com>
2021-01-23 05:25:16 +0800

    update userscript configs, and beautify code.

--- index.user.js
+++ index.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Simple Copymanga Downloader
 // @namespace    -
-// @version      0.1.0
+// @version      0.1.1
 // @description  沒什麼技術成分，非常暴力的下載器
 // @author       LianSheng
-// @match        https://www.copymanga.com/*
+// @include      https://www.copymanga.com/*
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jszip/3.5.0/jszip.min.js
 // @grant        GM_addStyle
 // @grant        GM_getValue
@@ -25,7 +25,7 @@ async function asyncForEach(array, callback) {
 
 // 主頁：控制是否爲選取模式，若是則不觸發連結
 function clickProxy(event, selectMode) {
-    if(selectMode){
+    if (selectMode) {
         event.preventDefault();
         event.stopPropagation();
 
@@ -35,15 +35,15 @@ function clickProxy(event, selectMode) {
 }
 
 // 主頁：下載所有已選，若 retry 爲 true 則只下載失敗的
-function downloadSelected(retry=false) {
+function downloadSelected(retry = false) {
     let allLi, allData;
 
-    if(retry){
+    if (retry) {
         allLi = document.querySelectorAll("div[id].active ul.table-all a li.selected.failed");
-        allData = [...allLi].map( li => [li.parentElement.getAttribute("title"), li.parentElement.getAttribute("href"), li]);
+        allData = [...allLi].map(li => [li.parentElement.getAttribute("title"), li.parentElement.getAttribute("href"), li]);
     } else {
         allLi = document.querySelectorAll("div[id].active ul.table-all a li.selected");
-        allData = [...allLi].map( li => [li.parentElement.getAttribute("title"), li.parentElement.getAttribute("href"), li]);
+        allData = [...allLi].map(li => [li.parentElement.getAttribute("title"), li.parentElement.getAttribute("href"), li]);
     }
 
     asyncForEach(allData, async data => {
@@ -53,13 +53,13 @@ function downloadSelected(retry=false) {
         await new Promise((resolve, reject) => {
             let count = 0;
             let id = setInterval(() => {
-                if(GM_getValue("downloading") == "completed"){
+                if (GM_getValue("downloading") == "completed") {
                     clearInterval(id);
                     resolve();
                 }
 
                 // 超時：600*100ms = 60s
-                if(count > 600) {
+                if (count > 600) {
                     clearInterval(id);
                     reject();
                 }
@@ -93,12 +93,14 @@ async function downloanThisEpisode(imgs) {
         let realSrc = img.getAttribute("data-src");
         await fetch(`${CORSProxy}${realSrc}`).then(
             r => r.blob()
-        ).then( image => {
+        ).then(image => {
             zip.file(`${index+1}.webp`, image);
         });
     });
 
-    await zip.generateAsync({type:"base64"}).then(
+    await zip.generateAsync({
+        type: "base64"
+    }).then(
         base64 => window.location = "data:application/zip;base64," + base64
     ).then(zip => {
         let link = document.createElement('a');
@@ -113,12 +115,12 @@ async function downloanThisEpisode(imgs) {
     window.close();
 }
 
-(function() {
+(function () {
     'use strict';
 
     GM_addStyle(`.selected { background-color: lightblue; } .success { background-color: lightgreen; } .failed { background-color: pink; }`);
 
-    if(Url.match(/\/comic\/[^\/]+$/)){
+    if (Url.match(/\/comic\/[^\/]+$/)) {
         // 特定漫畫選擇話次頁
 
         const Title = document.querySelector("h6").innerText;
@@ -128,26 +130,26 @@ async function downloanThisEpisode(imgs) {
             let episodeList = document.querySelectorAll("ul.table-all");
             let buttonAdded = false;
 
-            if(episodeList.length == 0)
+            if (episodeList.length == 0)
                 return;
             else
                 clearInterval(id);
 
-            episodeList.forEach( list => {
+            episodeList.forEach(list => {
                 // 個別清單
 
-                if(! buttonAdded) {
+                if (!buttonAdded) {
                     let field = document.querySelector(".table-default-right");
                     field.insertAdjacentHTML("afterbegin", `<span style="user-select: none; cursor: pointer; padding-top: 6px; padding-right: 2rem;"><input type="checkbox" id="cb_changeMode"><label for="cb_changeMode">&nbsp;單選模式</label><span style="width: 2rem; display: inline-block;"></span><input type="checkbox" id="cb_selectAll"><label for="cb_selectAll">&nbsp;全選</label><span style="width: 2rem; display: inline-block;"></span><button id="btn_download">下載選取</button><span style="width: 2rem; display: inline-block;"></span><button id="btn_retry">重試失敗的下載</button></span>`);
                     field.querySelector("#btn_download").onclick = () => {
                         let ok = confirm("請確認 pop-up 的權限已開啓，否則無法正常運作。\n（這個訊息每次都會顯示，無論是否已開啓）");
-                        if(ok){
+                        if (ok) {
                             downloadSelected();
                         }
                     };
                     field.querySelector("#btn_retry").onclick = () => {
                         let ok = confirm("請確認 pop-up 的權限已開啓，否則無法正常運作。\n（這個訊息每次都會顯示，無論是否已開啓）");
-                        if(ok){
+                        if (ok) {
                             downloadSelected(true);
                         }
                     };
@@ -156,12 +158,12 @@ async function downloanThisEpisode(imgs) {
                         let checked = e.target.checked;
                         let episodes = document.querySelectorAll("div[id].active ul.table-all a li");
 
-                        if(checked){
-                            episodes.forEach( ep => {
+                        if (checked) {
+                            episodes.forEach(ep => {
                                 ep.classList.add("selected");
                             });
                         } else {
-                            episodes.forEach( ep => {
+                            episodes.forEach(ep => {
                                 ep.classList.remove("selected");
                             });
                         }
@@ -170,7 +172,7 @@ async function downloanThisEpisode(imgs) {
                     buttonAdded = true;
                 }
 
-                list.childNodes.forEach( ep => {
+                list.childNodes.forEach(ep => {
                     // 個別話次
                     ep.onclick = e => {
                         clickProxy(e, selectMode);
@@ -178,11 +180,11 @@ async function downloanThisEpisode(imgs) {
                 });
             });
         }, 100);
-    } else if(Url.match(/\/comic\/[^\/]+\/chapter\/.+$/)){
+    } else if (Url.match(/\/comic\/[^\/]+\/chapter\/.+$/)) {
         let id = setInterval(() => {
             let allImg = document.querySelectorAll(".comicContent-image-all img");
 
-            if(allImg.length > 0){
+            if (allImg.length > 0) {
                 clearInterval(id);
                 downloanThisEpisode([...allImg]);
             }
